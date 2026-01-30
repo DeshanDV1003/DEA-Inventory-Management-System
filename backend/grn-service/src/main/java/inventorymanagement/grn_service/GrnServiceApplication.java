ad746453991e62069117f977713789a3e7620e62
W. Deshan Vimukthi De Silva <dvdsilva@students.nsbm.ac.lk>
2026-01-30 22:46:13 +0530

    Initial Commit to inventory management

---
+++ backend/grn-service/src/main/java/inventorymanagement/grn_service/GrnServiceApplication.java
@@ -0,0 +1,13 @@
+package inventorymanagement.grn_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrnServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrnServiceApplication.class, args);
+	}
+
+}
---
+++ backend/grn-service/src/test/java/inventorymanagement/grn_service/GrnServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package inventorymanagement.grn_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrnServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
