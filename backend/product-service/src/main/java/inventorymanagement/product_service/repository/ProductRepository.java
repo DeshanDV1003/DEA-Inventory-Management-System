f21ad23fd4f41549f4f831959f74d5ac66799e03
Elapatha2002 <pasinduelapatha02@gmail.com>
2026-02-23 15:24:08 +0530

    reverted Product service on user-service branch

--- backend/product-service/src/main/java/inventorymanagement/product_service/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package inventorymanagement.product_service.repository;
-
-import inventorymanagement.product_service.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-    Product findProductByProductId(Long productId);
-}
