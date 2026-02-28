2e2004852f7346275f67c0f48ab71cc9d54fec4f
Elapatha2002 <pasinduelapatha02@gmail.com>
2026-02-28 12:22:27 +0530

    product service re pushed

---
+++ backend/product-service - Copy/src/main/java/inventorymanagement/product_service/dto/AddProductRequestDto.java
@@ -0,0 +1,90 @@
+package inventorymanagement.product_service.dto;
+
+public class AddProductRequestDto {
+    private Integer warehouseId;
+    private Integer companyId;
+    private Integer supplierId;
+    private String name;
+    private Integer sku;
+    private String price;
+    private String imgPath;
+    private String status;
+
+    public AddProductRequestDto() {
+    }
+
+    public AddProductRequestDto(Integer warehouseId, Integer companyId, Integer supplierId, String name, Integer sku, String price, String imgPath, String status) {
+        this.warehouseId = warehouseId;
+        this.companyId = companyId;
+        this.supplierId = supplierId;
+        this.name = name;
+        this.sku = sku;
+        this.price = price;
+        this.imgPath = imgPath;
+        this.status = status;
+    }
+
+    public Integer getWarehouseId() {
+        return warehouseId;
+    }
+
+    public void setWarehouseId(Integer warehouseId) {
+        this.warehouseId = warehouseId;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public Integer getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Integer supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSku() {
+        return sku;
+    }
+
+    public void setSku(Integer sku) {
+        this.sku = sku;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
