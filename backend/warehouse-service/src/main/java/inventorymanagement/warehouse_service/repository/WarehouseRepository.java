a7bd870d0625e5ddec04041b986d65ab2c0159ed
Elapatha2002 <135606905+Elapatha2002@users.noreply.github.com>
2026-03-05 16:16:07 +0530

    Delete backend/warehouse-service directory

--- backend/warehouse-service/src/main/java/inventorymanagement/warehouse_service/repository/WarehouseRepository.java
+++
@@ -1,9 +0,0 @@
-package inventorymanagement.warehouse_service.repository;
-
-import inventorymanagement.warehouse_service.entity.Warehouse;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
-}
