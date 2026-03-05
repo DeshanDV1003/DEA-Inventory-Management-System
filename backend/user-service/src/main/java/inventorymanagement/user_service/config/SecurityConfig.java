8561894d285cca53fb5ac7c789e970fd7a3aa529
Elapatha2002 <135606905+Elapatha2002@users.noreply.github.com>
2026-03-05 10:35:51 +0530

    Update SecurityConfig.java

--- backend/user-service/src/main/java/inventorymanagement/user_service/config/SecurityConfig.java
+++ backend/user-service/src/main/java/inventorymanagement/user_service/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
