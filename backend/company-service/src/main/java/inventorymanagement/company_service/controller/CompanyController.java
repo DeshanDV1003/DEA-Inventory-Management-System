3c48621211d4ce8575d2833f05daf8700ded4772
Elapatha2002 <135606905+Elapatha2002@users.noreply.github.com>
2026-03-05 10:50:14 +0530

    Fix API endpoint path in CompanyController

--- backend/company-service/src/main/java/inventorymanagement/company_service/controller/CompanyController.java
+++ backend/company-service/src/main/java/inventorymanagement/company_service/controller/CompanyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "api/v1/companies")
+@RequestMapping(value = "/api/v1/companies")
 public class CompanyController {
 
     @Autowired
