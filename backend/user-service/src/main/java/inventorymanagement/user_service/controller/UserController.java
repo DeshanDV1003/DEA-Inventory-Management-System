c44b8f488d9fe2996794ed52fc068be6f024b706
Elapatha2002 <135606905+Elapatha2002@users.noreply.github.com>
2026-03-05 10:53:12 +0530

    Update UserController.java

--- backend/user-service/src/main/java/inventorymanagement/user_service/controller/UserController.java
+++ backend/user-service/src/main/java/inventorymanagement/user_service/controller/UserController.java
@@ -1,13 +1,11 @@
 package inventorymanagement.user_service.controller;
 
-import inventorymanagement.user_service.entity.User;
 import inventorymanagement.user_service.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
-@RequestMapping(value="/apii")
+@RequestMapping(value="/api/v1/users")
 public class UserController {
     @Autowired
     private UserServiceImpl userService;
