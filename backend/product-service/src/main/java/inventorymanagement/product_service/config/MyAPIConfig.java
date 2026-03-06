80944b7051fcca21923e64a31b3061d4938fc2d0
Elapatha2002 <135606905+Elapatha2002@users.noreply.github.com>
2026-03-06 10:13:05 +0530

    Update MyAPIConfig.java

--- backend/product-service/src/main/java/inventorymanagement/product_service/config/MyAPIConfig.java
+++ backend/product-service/src/main/java/inventorymanagement/product_service/config/MyAPIConfig.java
@@ -1,18 +1,27 @@
 package inventorymanagement.product_service.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyAPIConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Product API")
                         .version("1.0")
-                        .description("Product API desc"));
+                        .description("Product API desc"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .schemaRequirement("Bearer Authentication",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"));
     }
 }
